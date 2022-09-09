7597c466e2f22ab7ef35b70b52ed0dd0bae67980
Ochede simeon <ochedeadah@gmail.com>
2022-09-08 21:38:48 -0700

    T6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,30 +18,56 @@
 
 
 
-int main (void)
+int main(void)
 
 
 
 {
 
-	char c;
 
-	int i;
 
-long li;
+			char c;
 
-long long lli;
 
-float f;
 
-printf ("Size of a char: %ld byte(s)\n", sizeof(c));
+						int i;
 
-printf ("Size of a char: %ld byte(s)\n", sizeof(i));
 
-printf ("Size of a char: %ld byte(s)\n", sizeof(li));
 
-printf ("Size of a char: %ld byte(s)\n", sizeof(lli));
+										long li;
 
-printf ("Size of a char: %ld byte(s)\n", sizeof(f));
 
-return (0);
+
+															long long lli;
+
+
+
+																					float f;
+
+
+
+
+
+
+
+																												printf("Size of a char: %ld byte(s)\n", sizeof(c));
+
+
+
+																																				printf("Size of an int: %ld byte(s)\n", sizeof(i));
+
+
+
+																																													printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+
+
+
+																																																							printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+
+
+
+																																																																		printf("Size of a float: %ld byte(s)\n", sizeof(f));
+
+
+
+																																																																														return (0);
